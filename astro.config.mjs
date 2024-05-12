996c7273ab4b5725796aa023310550c42f56cc29
Smit Barmase <heysmitbarmase@gmail.com>
2024-05-12 22:18:44 +0530

    lmao

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [simpleStackStream()],
-	output: "servers",
+	output: "server",
 	adapter: vercel(),
 });
