44542964d9f274f7669d6b879d13797af146b791
Smit Barmase <heysmitbarmase@gmail.com>
2024-05-12 22:37:42 +0530

    make sense?

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
 	integrations: [simpleStackStream()],
 	output: "server",
-	adapter: vercel(),
+	adapter: vercel({
+		supportsResponseStreaming: true,
+	}),
 });
