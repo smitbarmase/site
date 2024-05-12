66816d4677998856aa1bda2b4c9fa4c74f7bc4fb
Smit Barmase <heysmitbarmase@gmail.com>
2024-05-12 22:55:36 +0530

    hahah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
-import simpleStackStream from "simple-stack-stream";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [simpleStackStream()],
-	output: "server",
-	adapter: vercel({
-		supportsResponseStreaming: true,
-	}),
+	integrations: [],
+	output: "hybrid",
+	adapter: vercel(),
 });
