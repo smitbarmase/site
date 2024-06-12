3e83d8551396106032caec39f6e884f3acb50c9a
Smit Barmase <heysmitbarmase@gmail.com>
2024-06-12 23:11:59 +0530

    style fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "selector",
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		fontFamily: {
-			sans: ["LibertinusSerif", "sans-serif"],
-		},
-		colors: {
-			// dark
-			gray: "#1b1b1b",
-			lightgray: "#323232",
-			white: "#efefef",
-			blue: "#0000ff",
-			// light
-			yellow: "#ffffdd",
-			darkyellow: "#e4e4ac",
-			black: "#0d0d0d",
-			lightblue: "#2f9bee",
-			// common
-			red: "#f00000",
-		},
-	},
-	plugins: [],
+  darkMode: "selector",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["LibertinusSerif", "sans-serif"],
+    },
+    colors: {
+      darkgray: "#1b1b1b", // dark background, light text
+      gray: "#323232", // dark border
+      lightblue: "#2f9bee", // dark link
+
+      white: "#efefef", // light background, dark text
+      lightgray: "#bfbfbf", // light border
+      blue: "#0000ff", // light link
+
+      red: "#f00000", // common
+      black: "#000000", // common
+    },
+  },
+  plugins: [],
 };
