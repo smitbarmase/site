632c9cc08b0b00161dea027cdf1701d835af2536
Smit Barmase <heysmitbarmase@gmail.com>
2024-06-19 01:47:57 +0530

    Revert "minify"
    This reverts commit 6ae4bbd997369e92e158025216de15291d531508.

--- src/scripts/theme.ts
+++ src/scripts/theme.ts
@@ -1,33 +1,43 @@
 const processTheme = () => {
-	const toggleThemeButton = document.getElementById("toggle-theme");
+  const body = document.body;
+  const toggleThemeButton = document.getElementById("toggle-theme");
+  const toggleThemeDesktopButton = document.getElementById(
+    "toggle-theme-desktop",
+  );
 
-	const isDarkTheme = () => document.documentElement.classList.contains("dark");
+  const isDarkTheme = () => body.classList.contains("dark");
 
-	const updateButton = () => {
-		if (toggleThemeButton) {
-			toggleThemeButton.textContent = isDarkTheme()
-				? "lights on"
-				: "lights off";
-		}
-	};
+  const updateButton = () => {
+    if (toggleThemeButton) {
+      toggleThemeButton.textContent = isDarkTheme()
+        ? "lights on"
+        : "lights off";
+    }
+    if (toggleThemeDesktopButton) {
+      toggleThemeDesktopButton.textContent = isDarkTheme()
+        ? "lights on"
+        : "lights off";
+    }
+  };
 
-	const toggleTheme = () => {
-		const isDark = isDarkTheme();
-		document.documentElement.classList.toggle("dark", !isDark);
-		localStorage.setItem("theme", isDark ? "light" : "dark");
-		updateButton();
-	};
+  const toggleTheme = () => {
+    const isDark = isDarkTheme();
+    body.classList.toggle("dark", !isDark);
+    localStorage.setItem("theme", isDark ? "light" : "dark");
+    updateButton();
+  };
 
-	// use button to toggle theme
-	if (toggleThemeButton) toggleThemeButton.onclick = toggleTheme;
+  // use button to toggle theme
+  if (toggleThemeButton) toggleThemeButton.onclick = toggleTheme;
+  if (toggleThemeDesktopButton) toggleThemeDesktopButton.onclick = toggleTheme;
 
-	// Use system theme by default
-	const prefersDark =
-		window.matchMedia &&
-		window.matchMedia("(prefers-color-scheme: dark)").matches;
-	const storedTheme = localStorage.getItem("theme");
-	const theme = storedTheme ? storedTheme : prefersDark ? "dark" : "light";
-	document.documentElement.classList.toggle("dark", theme === "dark");
-	updateButton();
+  // Use system theme by default
+  const prefersDark =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const storedTheme = localStorage.getItem("theme");
+  const theme = storedTheme ? storedTheme : prefersDark ? "dark" : "light";
+  body.classList.toggle("dark", theme === "dark");
+  updateButton();
 };
 processTheme();
