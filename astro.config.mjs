f4db0c7516696f199c467f2ca403cdf2de15543d
tims <0xtimsb@gmail.com>
2024-08-28 19:32:43 +0530

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://0xtimsb.com",
+  integrations: [sitemap()],
+});
