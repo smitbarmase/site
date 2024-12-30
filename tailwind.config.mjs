e7c0429e8e69358128398be47180b0c009de775f
tims <0xtimsb@gmail.com>
2024-12-30 09:43:56 +0530

    style update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 export default {
   content: ["./src/**/*.{astro,md,mdx,ts,tsx}"],
   theme: {
     extend: {
+      spacing: {
+        page: "3rem", // 12
+        "page-mobile": "1rem", // 4
+      },
       fontFamily: {
         geist: ["Geist", "sans-serif"],
+      },
+      colors: {
+        "nav-text": colors.stone[700],
+        "nav-text-hover": colors.stone[500],
+        "code-bg": colors.stone[50],
       },
     },
   },
