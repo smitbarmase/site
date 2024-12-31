843e35313eb3447e139a0c0fe9f22c0a69a07845
tims <0xtimsb@gmail.com>
2024-12-31 15:59:10 +0530

    update layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,7 +8,8 @@ const posts = defineCollection({
   loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: POSTS_DATA_DIR }),
   schema: z.object({
     title: z.string(),
-    sort: z.number().optional(),
+    type: z.enum(["writing"]).optional(),
+    date: z.date().optional(),
   }),
 });
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,16 +5,8 @@ export default {
   content: ["./src/**/*.{astro,md,mdx,ts,tsx}"],
   theme: {
     extend: {
-      spacing: {
-        page: "3rem", // 12
-        "page-mobile": "1rem", // 4
-      },
       fontFamily: {
         dejavu: ["DejavuSansMono", "monospace"],
-      },
-      colors: {
-        "nav-text": colors.stone[700],
-        "nav-text-hover": colors.stone[500],
       },
     },
   },
